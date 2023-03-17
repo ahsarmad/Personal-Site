91085b64e30b8f24d65f9768a81eb9dac87cfcd0
Sarmad <afzalsarmad77@gmail.com>
2023-03-16 22:42:20 -0400

    added personal links + small screen media queries

--- js/script.js
+++ js/script.js
@@ -324,3 +324,30 @@ function scrollUp() {
 }
 
 window.addEventListener("scroll", scrollUp);
+
+// Scroll Section Active highlight
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+
+  console.log("Section highlight working!");
+}
+window.addEventListener("scroll", scrollActive);
