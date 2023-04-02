7160815c80327ee0f426a4aeeaf37795015dcb8d
Sarmad <afzalsarmad77@gmail.com>
2023-04-02 22:40:13 +0300

    added splash screen effect

--- js/script.js
+++ js/script.js
@@ -388,4 +388,31 @@ images.forEach((image) => {
   observer.observe(image);
 });
 
-// loading
+// splash
+
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 100);
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
